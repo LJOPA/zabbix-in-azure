0bffb1db502f4008a36711c776ae32b4ea4bb1cb
Luke Opalinski <luke.opalinski@gmail.com>
2026-05-10 21:13:37 +0100

    feat: add azurerm_client_config data source and fix vnet name reference

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = module.naming.vnet_name
+  name                = module.naming.vnet.name
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   address_space       = ["10.0.0.0/16"]
