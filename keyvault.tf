7d09c207471d5a2899b953830fefad8af012cc24
Luke Opalinski <luke.opalinski@gmail.com>
2026-05-11 00:19:08 +0100

    fix: update tenant_id reference in key vault configuration

--- keyvault.tf
+++ keyvault.tf
@@ -3,7 +3,7 @@ resource "azurerm_key_vault" "key_vault" {
   location                    = azurerm_resource_group.resource_group.location
   resource_group_name         = azurerm_resource_group.resource_group.name
   enabled_for_disk_encryption = true
-  tenant_id                   = azurerm_user_assigned_identity.example.tenant_id
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = true
 
