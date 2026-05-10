7c55110ce70c43fac39fc037b086c9af95403ac9
Luke Opalinski <luke.opalinski@gmail.com>
2026-05-10 21:14:44 +0100

    fix: update virtual network resource name for consistency

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = module.naming.vnet.name
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = module.naming.virtual_network.name
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   address_space       = ["10.0.0.0/16"]
